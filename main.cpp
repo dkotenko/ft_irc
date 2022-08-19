4927954f22a70793f2aa5b78c406a84b4ac51dbf
Redwyn Poetess <rpoetess@oa-d5.msk.21-school.ru>
2022-08-19 15:53:05 +0300

    add mainloop

--- main.cpp
+++ main.cpp
@@ -5,16 +5,12 @@
 #include <unistd.h> // For read
 #include <cerrno>
 
-int main(int argc, char **argv) {
-
+void server() {
     // Create a socket (IPv4, TCP)
-    (void)argc;
-    (void)argv;
-
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
-    std::cout << "Failed to create socket. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
+        std::cout << "Failed to create socket. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
     }
 
     // Listen to port 9999 on any address
@@ -22,36 +18,58 @@ int main(int argc, char **argv) {
     sockaddr.sin_family = AF_INET;
     sockaddr.sin_addr.s_addr = INADDR_ANY;
     sockaddr.sin_port = htons(9999); // htons is necessary to convert a number to
-                                   // network byte order
+    // network byte order
     if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) {
-    std::cout << "Failed to bind to port 9999. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
+        std::cout << "Failed to bind to port 9999. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
     }
 
     // Start listening. Hold at most 10 connections in the queue
     if (listen(sockfd, 10) < 0) {
-    std::cout << "Failed to listen on socket. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
+        std::cout << "Failed to listen on socket. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
     }
 
     // Grab a connection from the queue
     int addrlen = sizeof(sockaddr);
     int connection = accept(sockfd, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen);
     if (connection < 0) {
-    std::cout << "Failed to grab connection. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
+        std::cout << "Failed to grab connection. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
     }
 
-    // Read from the connection
-    char buffer[100];
-    read(connection, buffer, 100);
-    std::cout << "The message was: " << buffer;
-
-    // Send a message to the connection
-    std::string response = "Good talking to you\n";
-    send(connection, response.c_str(), response.size(), 0);
-
+    while (true) {
+        // Read from the connection
+        char buffer[100];
+        read(connection, buffer, 100);
+        if (strcmp(buffer, "QUIT\n") == 0) {
+            break;
+        }
+        std::cout << "The message was: " << buffer;
+        // Send a message to the connection
+        //std::string response = "Good talking to you\n";
+        //send(connection, response.c_str(), response.size(), 0);
+        bzero(buffer, 100);
+    }
     // Close the connections
     close(connection);
     close(sockfd);
+}
+
+int validation(int argc, char **argv) {
+    if (argc != 3) {
+        std::cout<<"Incorrect format";
+        std::cout<<"Correct format:./ft_irc <port> <password>";
+        return 1;
+    }
+    //std::cout<<argv[1]<<" "<<argv[2]<<"\n";
+    (void)argv;
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    if (validation(argc, argv)) {
+        exit(0);
+        }
+    server();
 }
