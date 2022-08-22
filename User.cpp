33b56b2bac46e0b15939efafcc41035516bf2535
Redwyn Poetess <rpoetess@oa-f4.msk.21-school.ru>
2022-08-22 22:36:14 +0300

    User class

---
+++ User.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Redwyn Poetess on 22.08.2022.
+//
+
+#include "User.hpp"
+#include <string>
+#include "map"
+
+struct message {
+    int fd_from;
+    vector<int> fd_to;
+    string operation;
+    string data;
+};
+
+string pass
+
+map <string, vector<string>> users;
+
+User::User(string nick, string password) {
+    if (!users.find(nick)) {
+        if
+    }
+}
+
+void setUser(string nick) {
+
+}
---
+++ User.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Redwyn Poetess on 22.08.2022.
+//
+
+#ifndef FT_IRC_USER_H
+#define FT_IRC_USER_H
+
+
+class User {
+
+};
+
+
+#endif //FT_IRC_USER_H
