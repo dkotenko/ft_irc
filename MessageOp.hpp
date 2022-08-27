143318f330928c90168b4f9e526c658bbdb1418e
denis g <gavrilin94@mail.ru>
2022-08-27 14:26:57 +0300

    commit

---
+++ MessageOp.hpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include "utils.hpp"
+
+class Message
+{
+	private:
+		std::string						prefix;
+		std::string						command;
+		std::vector<std::string>		parameters;
+
+		Message();
+		Message(const Message& copy);
+		Message	&operator=(const Message& other);
+	public:
+		Message(const std::string &str);
+		virtual ~Message();
+		const std::string				&getPrefix() const;
+		const std::string				&getCmd() const;
+		const std::vector<std::string>	&getParams() const;
+}
